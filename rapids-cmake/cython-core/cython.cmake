45a7505eb3d2db5b3f8135ed00c462bc5f17a02c
Vyas Ramasubramani <vyasr@nvidia.com>
2023-06-28 17:50:29 -0700

    Check if defined

--- rapids-cmake/cython-core/cython.cmake
+++ rapids-cmake/cython-core/cython.cmake
@@ -141,10 +141,10 @@ function(rapids_cython_compile)
 
   # Handle prefix separately
   set(target_prefix)
-  if(${_RAPIDS_COMPILE_TARGET_PREFIX})
+  if(DEFINED _RAPIDS_COMPILE_TARGET_PREFIX)
     set(target_prefix ${_RAPIDS_COMPILE_TARGET_PREFIX})
   endif()
-  message("The target prefix for cython is ${_RAPIDS_COMPILE_TARGET_PREFIX}), ${target_prefix}")
+  message("The target prefix for cython is ${_RAPIDS_COMPILE_TARGET_PREFIX}, ${target_prefix}")
 
   # Now handle the multi-value args. These generally need different treatments
   if(NOT DEFINED _RAPIDS_COMPILE_SOURCE_FILES)
